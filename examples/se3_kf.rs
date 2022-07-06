b38d66d279b06226129e59ef5a439dbbe12c09f3
wangxiaochuTHU <wangxiaochu@tsinghua.org.cn>
2022-07-06 21:20:25 +0800

    Add SO3 SE3 examples

--- examples/se3_kf.rs
+++ examples/se3_kf.rs
@@ -99,8 +99,8 @@ pub mod seekf {
     use yakf::lie::se3::{combine, decombine, hat, jac_r, Alg6, Grp6, One2OneMapSE, Vec6, SE3};
     use yakf::lie::so3::{hat as hat3, Vec3};
 
-    use crate::linalg::allocator::Allocator;
-    use crate::linalg::{Const, DefaultAllocator, DimName, OMatrix, OVector, U3, U4, U6};
+    use se3_kf::linalg::allocator::Allocator;
+    use se3_kf::linalg::{Const, DefaultAllocator, DimName, OMatrix, OVector, U3, U4, U6};
     pub struct SEEKF {
         pub state: SE3,
         pmatrix: OMatrix<f64, U6, U6>,
